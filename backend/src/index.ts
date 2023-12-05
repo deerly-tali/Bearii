107a355116e3879dc0f02dfb47dda88dc202b1d9
Talia Martin <67079575+deerly-tali@users.noreply.github.com>
2023-12-04 21:34:29 -0500

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const app = express();
 
-console.log(
-  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
-);
+app.get("/hello", (req,res,next) => {
+    console.log("HELLO");
+});
 
 app.listen(8080, () => console.log("Running on port 8080"));
